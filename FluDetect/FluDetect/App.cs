84702b80c7fb64dcf09759c777bd7eb97cb0f833
Đoàn Thanh An <on Thanh An>
2016-05-26 21:57:54 +1000

    First commit

---
+++ FluDetect/FluDetect/App.cs
@@ -0,0 +1,35 @@
+﻿using FluDetect.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace FluDetect
+{
+    public class App : Application
+    {
+        public App()
+        {
+            // The root page of your application
+            MainPage = new MakingAppointment();
+            
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
